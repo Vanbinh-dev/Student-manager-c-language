bdda22a9f3f7d94e092a99be891dd45bd19b9513
Nguyen Van Binh <nguyenvanbinh542007@gmail.com>
2025-10-20 08:35:53 +0700

    Them 2 file kiem tra

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+bool intNumFormatable(char *numStr) {   
+    int i = (numStr[0] == '-') ? 1 : 0;
+    for (i; i < strlen(numStr); i++)
+        if (numStr[i] < 48 || numStr[i] > 57)
+            return false;
+
+    return true;
+}
+
+bool doubleNumFormatable(char *numStr) {
+    short dots = 0;
+    int i = (numStr[0] == '-') ? 1 : 0;
+
+    for (i; i < strlen(numStr); i++) {
+        if (numStr[i] == '.') {
+            dots++;
+            continue;
+        }
+
+        if (dots >= 2 || str[i] < 48 || str[i] > 57)
+            return false;
+    }
+
+    return true;
+}
 
 int main() {
     return 0;
